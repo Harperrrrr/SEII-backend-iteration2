26f4466eb865dcae26245b908d27fb59c7bc76b2
Harper <211250030@smail.nju.edu.cn>
2023-06-29 10:41:37 +0800

    initializa

---
+++ src/main/java/org/fffd/l23o6/dao/RouteDao.java
@@ -0,0 +1,9 @@
+package org.fffd.l23o6.dao;
+
+import org.fffd.l23o6.pojo.entity.RouteEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface RouteDao extends JpaRepository<RouteEntity, Long>{
+}
